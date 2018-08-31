a9bea4b5facf782bc3c241d342396727d4c13a91
setcookie <set@cooki.me>
2018-08-31 16:07:48 +0200

    updates phar

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-php=$(which php)
+PHP=$(which php)
 
-$php -d phar.readonly=0 vendor/bin/phar-builder package composer.json -f -n -o dist
+#parse command line arguments
+for i in "$@"
+do
+case $i in
+    --php=*)
+    PHP="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+    # unknown option
+    ;;
+esac
+done;
+
+$PHP -d phar.readonly=0 vendor/bin/phar-builder package composer.json -f -n -o dist
--- dist.sh
+++ dist.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-php -d phar.readonly=0 vendor/bin/phar-builder package composer.json -z -o ./dist
+PHP=$(which php)
+
+#parse command line arguments
+for i in "$@"
+do
+case $i in
+    --php=*)
+    PHP="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+    # unknown option
+    ;;
+esac
+done;
+
+$PHP -d phar.readonly=0 vendor/bin/phar-builder package composer.json -z -o ./dist
